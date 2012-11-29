22d7309d65e20670722a23ea4707f0fa42b97abc
Xavier Deschuyteneer <xavier.deschuyteneer@gmail.com>
2012-11-29 11:42:18 +0100

    ajout de la liste des permissions pour chaque fichier

--- my_ls.c
+++ my_ls.c
@@ -1,11 +1,16 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
+#include <locale.h>
+#include <langinfo.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+#include <xlocale.h>
 #include <stdlib.h>
-#include <strings.h>
-#include <sys/types.h> 
-#include <dirent.h> 
-#include <sys/stat.h> 
-#include <time.h> 
-
 /*
  * attributs
  */
@@ -23,41 +28,106 @@ const int BLACK = 30;
 const int RED = 31;
 const int GREEN = 32;
 const int YELLOW = 33;
-const int Blue = 34;
+const int BLUE = 34;
 const int MAGENTA = 35;
 const int CYAN = 36;
 const int WHITE = 37;
 
 void cprint(char* mot, int style, int color) {
-  printf("%c[%d;%dm%s%c[%dm\n", 27, style, color, mot, 27, 0);
+  printf("%c[%d;%dm%s%c[%dm", 27, style, color, mot, 27, 0);
+}
+
+char digit_to_char(int digit) {
+  char s[2];
+  sprintf(s, "%d", digit);
+  return s[0];
+}
+
+void printnbr(int nums[], char repr[], int size) {
+  int i;
+  for (i = 0; i < size; i++) {
+    repr[i] = digit_to_char(nums[i]);
+  }
+}
+
+int arrToInt(int nums[], int base, int cnt) { 
+  int tmp = 0; 
+  for (cnt = cnt - 1; cnt >= 0; cnt--) 
+    { 
+      tmp += nums[cnt] * pow(base, cnt); 
+    } 
+  return tmp; 
+} 
+
+void b10_to_bX(int num, int nums[], int base, int bits) {
+  int cnt = bits - 1; 
+  while (cnt < bits && cnt >= 0) { 
+    if (num != 0) {
+      nums[cnt] = num % base; 
+      num = num / base;
+    } else {
+      nums[cnt] = 0;
+    }
+    cnt--;
+  }
 }
 
 char* lister(char* loc) {
-  DIR *rep = opendir(loc); 
- 
+  DIR *rep = opendir(loc);
   if (rep != NULL) 
     { 
-      struct dirent *lecture; 
- 
-      while ((lecture = readdir (rep))) 
-        { 
-          struct stat st; 
- 
-          stat (lecture->d_name, &st);
-          time_t t = st.st_mtime; 
-          struct tm tm = *localtime (&t); 
-          char s[32]; 
-          strftime (s, sizeof s, "%d/%m/%Y %H:%M:%S", &tm); 
- 
-          printf ("%-14s %s\n", lecture->d_name, s);
-        } 
-      closedir (rep), rep = NULL; 
-    } 
-}
+      struct dirent  *dp;
+      struct stat     statbuf;
+      struct passwd  *pwd;
+      struct group   *grp;
+      struct tm      *tm;
+      char            datestring[256]; 
+      int nums[6];
+      int bin[3];
+      char repr[6];
+      int i;
+      int j;
+      while ((dp = readdir (rep))) { 
+        /* Get entry's information. */
+        if (stat(dp->d_name, &statbuf) == -1)
+          continue;
 
-int main(void)
-{
-  cprint("hello, world!", BOLD, RED);
-  lister(".");
-  return 0;
-}
+        b10_to_bX(statbuf.st_mode, nums, 8, 6);
+        printf("|");
+        if (nums[0] == 0 && nums[1] == 4 && nums[2] == 0) {
+          printf("d");
+        } else if (nums[0] == 1 && nums[1] == 0 && nums[2] == 0) {
+          printf("-");
+        }
+        for (i = 3; i < 6; i++) {
+          b10_to_bX(nums[i], bin, 2, 3);
+          if (bin[0] == 0) {
+            printf("-");
+          } else {
+            cprint("r", BOLD, GREEN);
+          }
+          if (bin[1] == 0) {
+            printf("-");
+          } else {
+            cprint("w", BOLD, BLUE);
+          }
+          if (bin[2] == 0) {
+            printf("-");
+          } else {
+            cprint("x", BOLD, RED);
+          }
+        }
+        printf("| - %s\n", dp->d_name);
+        
+      } 
+        closedir (rep), rep = NULL; 
+      } 
+    }
+
+
+
+  int main(void){
+    cprint("hello, world!\n", BOLD, RED);
+    lister(".");
+    return 0;
+  }
